9544b52284d3a3241cb359bf986169989ad776aa
Alper ARI <alper.hamza.ari@gmail.com>
2022-11-13 21:11:37 +0300

    Add and apply Prettier to the project

--- routes/test.js
+++ routes/test.js
@@ -1,15 +1,14 @@
 //jshint esversion:6
-const express = require("express");
+const express = require('express');
 
 const router = express.Router();
 
 router.get('/test', (req, res) => {
-    res.send("Test is successful!");
+  res.send('Test is successful!');
 });
 
 router.get('/badRequest', (req, res) => {
-    res.sendStatus(400);
+  res.sendStatus(400);
 });
-
 
 module.exports = router;
