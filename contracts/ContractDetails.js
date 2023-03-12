a35b34b51f111fce382f2fba7342f2c7114fdb4d
Alper ARI <alper.hamza.ari@gmail.com>
2023-03-12 18:50:14 +0300

    Update MarketContract, modify POST /market/mint

--- contracts/ContractDetails.js
+++ contracts/ContractDetails.js
@@ -1,7 +1,7 @@
 const ContractAddress =
   '0x097e4e76369d37dD15B2D5f557D5982F2c723b85'.toLocaleLowerCase();
 const MarketContractAddress =
-  '0xFB94A6A9E4C4Bd1A7e8b1c6b4EC54b53378D1F1F'.toLocaleLowerCase();
+  '0x0D08BAa434f30af491Fd94030359e6F34402A417'.toLocaleLowerCase();
 module.exports = {
   ContractAddress,
   MarketContractAddress,
