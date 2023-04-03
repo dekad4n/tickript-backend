2d66af2f479e4520058c8f464d82311d332a915d
Alper ARI <alper.hamza.ari@gmail.com>
2023-04-03 12:33:03 +0300

    Create auction route

--- contracts/ContractDetails.js
+++ contracts/ContractDetails.js
@@ -2,7 +2,10 @@ const ContractAddress =
   '0xE3cdd13A3a59CDc087A88015763b1d8f9A7D7642'.toLocaleLowerCase();
 const MarketContractAddress =
   '0xc087A3d76e0E7b94421F5CCaA9E4Af0658C3106F'.toLocaleLowerCase();
+const AuctionContractAddress = '0x0'.toLocaleLowerCase();
+
 module.exports = {
   ContractAddress,
   MarketContractAddress,
+  AuctionContractAddress,
 };
