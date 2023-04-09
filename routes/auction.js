bac99d21e675be8dd316cc913208244af6517953
Sadi Gülbey <sadigulbey@sabanciuniv.edu>
2023-04-09 16:06:11 +0300

    auction functions

--- routes/auction.js
+++ routes/auction.js
@@ -67,4 +67,170 @@ router.post('/create-bid-item', auth, async (req, res) => {
   }
 });
 
+router.post('/stop-auction', auth, async (req, res) => {
+  const { auctionId } = req.body;
+
+  if (!auctionId) {
+    res.status(400);
+    res.json({ message: 'Inputs are not valid' });
+    return;
+  }
+
+  try {
+    transaction = await auctionContract.methods
+      .StopAuction(auctionId)
+      .encodeABI();
+    let transactionParameters = {
+      from: req.user.publicAddress,
+      to: ContractDetails.AuctionContractAddress,
+    };
+    transactionParameters['data'] = transaction;
+
+    console.log(transactionParameters);
+    res.json(transactionParameters);
+    return;
+  } catch (err) {
+    console.log(err);
+    res.status(500);
+    res.json({ message: 'Failed to stop auction' });
+    return;
+  }
+});
+
+router.get('/get-auction', auth, async (req, res) => {
+  const { auctionId } = req.query;
+
+  if (!auctionId) {
+    res.status(400);
+    res.json({ message: 'Inputs are not valid' });
+    return;
+  }
+
+  try {
+    const auction = await auctionContract.methods
+      .GetAuctionInfo(auctionId)
+      .call();
+    res.json(auction);
+    return;
+  } catch (err) {
+    console.log(err);
+    res.status(500);
+    res.json({ message: 'Failed to get auction' });
+    return;
+  }
+});
+
+router.get('/list-prev-bids', auth, async (req, res) => {
+  const { auctionId } = req.query;
+
+  if (!auctionId) {
+    res.status(400);
+    res.json({ message: 'Inputs are not valid' });
+    return;
+  }
+
+  try {
+    const prevBids = await auctionContract.methods
+      .listPrevBids(auctionId)
+      .call();
+    res.json(prevBids);
+    return;
+  } catch (err) {
+    console.log(err);
+    res.status(500);
+    res.json({ message: 'Failed to get auction' });
+    return;
+  }
+});
+
+router.post('/bid', auth, async (req, res) => {
+  const { auctionId, bidPrice } = req.body;
+
+  if (!auctionId) {
+    res.status(400);
+    res.json({ message: 'Inputs are not valid' });
+    return;
+  }
+
+  try {
+    transaction = await auctionContract.methods.placeBid(auctionId).encodeABI();
+    let transactionParameters = {
+      from: req.user.publicAddress,
+      to: ContractDetails.AuctionContractAddress,
+      value:
+        bidPrice ?? web3.utils.toWei(bidPrice.toString(), 'ether').toString(),
+    };
+    transactionParameters['data'] = transaction;
+
+    console.log(transactionParameters);
+    res.json(transactionParameters);
+    return;
+  } catch (err) {
+    console.log(err);
+    res.status(500);
+    res.json({ message: 'Failed to bid' });
+    return;
+  }
+});
+
+router.post('/finish-bid', auth, async (req, res) => {
+  const { auctionId } = req.body;
+
+  if (!auctionId) {
+    res.status(400);
+    res.json({ message: 'Inputs are not valid' });
+    return;
+  }
+
+  try {
+    transaction = await auctionContract.methods
+      .finishBid(auctionId)
+      .encodeABI();
+    let transactionParameters = {
+      from: req.user.publicAddress,
+      to: ContractDetails.AuctionContractAddress,
+    };
+    transactionParameters['data'] = transaction;
+
+    console.log(transactionParameters);
+    res.json(transactionParameters);
+    return;
+  } catch (err) {
+    console.log(err);
+    res.status(500);
+    res.json({ message: 'Failed to end auction' });
+    return;
+  }
+});
+
+router.post('/payback-prev-bids', auth, async (req, res) => {
+  const { auctionId } = req.body;
+
+  if (!auctionId) {
+    res.status(400);
+    res.json({ message: 'Inputs are not valid' });
+    return;
+  }
+
+  try {
+    transaction = await auctionContract.methods
+      .payBackPrevBids(auctionId)
+      .encodeABI();
+    let transactionParameters = {
+      from: req.user.publicAddress,
+      to: ContractDetails.AuctionContractAddress,
+    };
+    transactionParameters['data'] = transaction;
+
+    console.log(transactionParameters);
+    res.json(transactionParameters);
+    return;
+  } catch (err) {
+    console.log(err);
+    res.status(500);
+    res.json({ message: 'Failed to payback previous bids' });
+    return;
+  }
+});
+
 module.exports = router;
